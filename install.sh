166033bc2058725747b9512a0e5274f7936a8200
Murilo Magalhaes <murilomagalhaes@outlook.com>
2021-05-02 18:44:15 -0300

    First commit

---
+++ install.sh
@@ -0,0 +1,52 @@
+# Update Packages
+apt update
+
+# Upgrade Packages
+apt upgrade
+
+# Install Git
+apt install -y git
+
+# Install Apache
+apt install -y apache2
+
+# Enable Apache Mods
+a2enmod rewrite
+
+# Install PHP
+apt install -y php
+
+# PHP Apache Mod
+apt install -y libapache2-mod-php
+
+# Restart Apache
+service apache2 restart
+
+# Install PHP Mods for working with most MVC frameworks
+apt install -y php-common
+apt install -y php-mcrypt
+apt install -y php-zip
+apt install -y php-dev
+apt install -y php-json
+apt install -y php-xml
+apt install -y php-curl
+apt install -y php-intl
+apt install -y php-mbstring
+apt install -y php-bcmath
+apt install -y php-mysql
+
+# Install PHP Composer (Latest)
+curl -Ss https://getcomposer.org/installer | php
+mv composer.phar /usr/bin/composer
+
+# Install NodeJS
+apt install -y nodejs
+apt install -y npm
+
+# Install MYSQL
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Mysql@vagrant'
+apt install -y mysql-server
+
+# Restart Apache
+sudo service apache2 restart
